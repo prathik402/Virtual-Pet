a98de5f280c4444626b118dc9aeab6810b3c9998
prathik402 <73998548+prathik402@users.noreply.github.com>
2021-08-09 15:34:47 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -22,10 +22,31 @@ function setup() {
  database = firebase.database();
  foodStock = database.ref("Food");
  foodStock.on("value",readStock);
+
+ feed=createButton("Feed the dog");
+ feed.position(700,95);
+ feed.mousePressed(feedDog);
+
+ addFood = createButton("Add food");
+ addFood.position(800,95);
+ addFood.mousePressed(foodStock = foodStock + 1);
 }
 
 function draw() {  
 background(46, 139, 87);
+
+fill(255,255,254);
+textSize(15);
+if(lastFed>=12){
+  text("Last Feed : " + lastFed%12 + " PM" ,350,30);
+}else if(lastFed==0){
+  text("Last Feed : 12 AM",350,30);
+}else{
+  text("Last Feed : "+ lastFed + "AM",350,30);
+}
+
+
+
 
 
 if(keyWentDown(UP_ARROW)){
@@ -36,9 +57,7 @@ if(keyWentDown(UP_ARROW)){
 
   drawSprites();
   //add styles here
-  textSize(17)
-  stroke("red")
-  text("Note: Press UP ARROW key to feed the dog milk", 100, 50)
+ 
   
 }
 function readStock(data){
@@ -55,6 +74,15 @@ if(x <= 0){
 else{
   x = x - 1
 }
+}
+function feedDog(){
+  dog.addImage(happyDogImg)
+
+  foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  database.ref("/").update({
+  Food:foodObj.getFoodStock(),
+  FeedTime:hour()
+  })
 }
 
 
